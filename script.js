59fe79b891b42511b3ddb5d77055c480018b4753
Yash Kumar <85291913+yashsaini7060@users.noreply.github.com>
2023-11-16 21:42:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+var i=1
+ 
+  while(i<20)
+  {
+    if(i%3==0 && i%5==0)
+        alert(`${i}\nFizzBuzz`)
+    else if(i%5==0)
+        alert(`${i}\nBuzz`)
+    else if(i%3==0)
+        alert(`${i}\nFizz`)
+    i++;
+  }
